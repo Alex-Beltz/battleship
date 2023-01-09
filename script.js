2ad0a9e2b5fed1e30bea672261e55dfb1ef57eaf
Alex Beltz <beltz.a@outlook.com>
2023-01-09 18:10:41 +0800

    Save point
    Still working out the randome placment of shipd, but want to save here.
    added a test.js file to structure my js different and compare

--- script.js
+++ script.js
@@ -79,99 +79,90 @@ function startGame() {
   createBoard(userGrid, userSquares);
   createBoard(compGrid, compSquares);
 
-  const shipsArray = [
-    {
-      name: "carrier",
-      size: [0, 1, 2, 3, 4],
-    },
-    {
-      name: "battleship",
-      size: [0, 1, 2, 3],
-    },
-    {
-      name: "cruiser",
-      size: [0, 1, 2],
-    },
-    {
-      name: "submarine",
-      size: [0, 1, 2],
-    },
-    {
-      name: "destroyer",
-      size: [0, 1],
-    },
-  ];
-
   function* randomPosition(squares) {
+    let shipsArray = [
+      {
+        name: "carrier",
+        spaces: [],
+        size: 5,
+      },
+      {
+        name: "battleship",
+        spaces: [],
+        size: 4,
+      },
+      {
+        name: "cruiser",
+        spaces: [],
+        size: 3,
+      },
+      {
+        name: "submarine",
+        spaces: [],
+        size: 3,
+      },
+      {
+        name: "destroyer",
+        spaces: [],
+        size: 2,
+      },
+    ];
+    // let shipLengths = shipsArray.spaces.map((ship) => ship.spaces);
+    // shipLengths;
+    // function taken(nums) {
+    //   squares.splice(nums);
+    // }
     let elem,
       len = squares.length;
     while (len > 0) {
-      let random = Math.floor(Math.random() * len);
-      elem = squares.splice(random, 1)[0];
-      yield elem;
-      len = squares.length;
-    }
-  }
-  const hidden = randomPosition(compSquares);
-  // compShips.add(hidden.next());ay
-  // compShips.add(hidden.next());
-  compShips.push(hidden.next());
-  compShips.push(hidden.next());
+      let rN = Math.floor(Math.random() * len);
+      let xORy = Math.random() < 0.5 ? x : y;
 
-  for (let i = 0; i < compShips.length; i++) {
-    // let item = compShips.values;
-    // item.forEach(classList.add('hidden-ship'))
-    let item = compShips[i].value;
-    item.classList.add("hidden-ship");
-  }
-
-  function placeShip(position, ship) {
-    if (position.dataset(parseInt.toString().slice(-1)) < `6`) {
-      // let loopShip = 'let i = 0; i < ship.size.length; i++';
-      for (let i = 0; i < ship.size.length; i++) {
-        console.log(ship.size.length);
-        position.nextElementSibling().classList.add("hidden-ship");
-      }
-    } else {
-      for (let i = 0; i < ship.size.length; i++) {
-        position.previousElementSibling().classList.add("hidden-ship");
+      if ((xORy = x && rN % 10 >= 4)) {
+        shipsArray[0].spaces.push(rN, rN + 1, rN + 2, rN + 3, rN + 4);
+        shipsArray[1].spaces.push(rN, rN + 1, rN + 2, rN + 3);
+        shipsArray[2].spaces.push(rN, rN + 1, rN + 2);
+        shipsArray[3].spaces.push(rN, rN + 1, rN + 2);
+        shipsArray[4].spaces.push(rN, rN + 1);
+      } else if ((xORy = x && rN % 10 <= 5)) {
+        shipsArray[0].spaces.push(rN, rN - 1, rN - 2, rN - 3, rN - 4);
+        shipsArray[1].spaces.push(rN, rN - 1, rN - 2, rN - 3);
+        shipsArray[2].spaces.push(rN, rN - 1, rN - 2);
+        shipsArray[3].spaces.push(rN, rN - 1, rN - 2);
+        shipsArray[4].spaces.push(rN, rN - 1);
+      } else if ((xORy = y && rN <= 49)) {
+        shipsArray[0].spaces.push(rN, rN + 10, rN + 20, rN + 30, rN + 40);
+        shipsArray[1].spaces.push(rN, rN + 10, rN + 20, rN + 30);
+        shipsArray[2].spaces.push(rN, rN + 10, rN + 20);
+        shipsArray[3].spaces.push(rN, rN + 10, rN + 20);
+        shipsArray[4].spaces.push(rN, rN + 10);
+      } else if ((xORy = y && rN >= 50)) {
+        shipsArray[0].spaces.push(rN, rN - 10, rN - 20, rN - 30, rN - 40);
+        shipsArray[1].spaces.push(rN, rN - 10, rN - 20, rN - 30);
+        shipsArray[2].spaces.push(rN, rN - 10, rN - 20);
+        shipsArray[3].spaces.push(rN, rN - 10, rN - 20);
+        shipsArray[4].spaces.push(rN, rN - 10);
       }
     }
   }
-  placeShip(compShips[0], shipsArray[0]);
-  placeShip(compShips[1], shipsArray[1]);
-  placeShip(compShips[2], shipsArray[2]);
-  placeShip(compShips[3], shipsArray[3]);
-  placeShip(compShips[4], shipsArray[4]);
 
-  //   } else if ((compShips[0].data-id(parseInt.toString().slice(-1))  calc() 9) {
-  //     for(let i = 0; i < shipsArray[0].size.length; i++) {
+  randomPosition(compSquares);
+  compSquares.length;
+  // const hidden = randomPosition(compSquares);
+  // // compShips.add(hidden.next());ay
+  // // compShips.add(hidden.next());
+  // compShips.push(hidden.next());
+  // compShips.push(hidden.next());
 
-  //     }
-  //   }
+  // for (let i = 0; i < compShips.length; i++) {
 
-  // randomPosition(hidden1)
-  // randomPosition(hidden2)
-
-  // console.log(randomPosition.dataset.id);
-  // hidden1 = compShips.push(randomPosition.dataset.id);
-
-  // gameSquares.addEventListener('click', () => {
-  //   if (gameSquares.dataset.id != hidden1.dataset.id) {
-  //     userMisses.push(gameSquares.dataset.id)
-  //     console.log(userMisses);
-  //   }
-  // });
-
-  // let hidden1 = "";
-  // let hidden2 = "";
-  // function randomPosition (ship) {
-  //   ship = gameSquares[Math.floor(Math.random() * gameSquares.length)];
-
-  //   ship = compShips.push(.dataset.id);
+  //   let item = compShips[i].value;
+  //   item.classList.add("hidden-ship");
   // }
-  // randomPosition(hidden1);
-  // randomPosition(hidden2);
+
+  function shipAlign(randomGenNum) {
+    // if thisNum data.id ends in <4 increment shipsArray
+  }
 }
 startGame();
 // document.addEventListener("load", startGame());
---
+++ test.js
@@ -0,0 +1,180 @@
+function startGame() {
+  const userGrid = document.getElementById("user-grid");
+  const compGrid = document.getElementById("comp-grid");
+
+  const userSquares = [];
+  const compSquares = [];
+  // console.log(userSquares);
+
+  const compShips = [];
+
+  // console.log(compShips);
+
+  const userMisses = [];
+  const userHits = [];
+  const compMisses = [];
+  const compHits = [];
+  const sunk = [];
+
+  function createBoard(grid, squares) {
+    for (let i = 0; i < 100; i++) {
+      const square = document.createElement(`div`);
+      square.dataset.id = i;
+      //   console.log(i);
+      let getLastIDNum = parseInt(i.toString().slice(-1));
+
+      if (i <= 9) {
+        square.innerHTML = `<h6 style="user-select: none">A-${i + 1}</h6>`;
+      } //******* "A" row****/
+
+      if (i > 9 && i <= 19) {
+        let n = getLastIDNum;
+        square.innerHTML = `<h6 style="user-select: none">B-${n + 1}</h6>`;
+      } //******* "B" row****/
+
+      if (i > 19 && i <= 29) {
+        let n = getLastIDNum;
+        square.innerHTML = `<h6 style="user-select: none">C-${n + 1}</h6>`;
+      } //******* "C" row****/
+
+      if (i > 29 && i <= 39) {
+        let n = getLastIDNum;
+        square.innerHTML = `<h6 style="user-select: none">D-${n + 1}</h6>`;
+      } //******* "D" row****/
+
+      if (i > 39 && i <= 49) {
+        let n = getLastIDNum;
+        square.innerHTML = `<h6 style="user-select: none">E-${n + 1}</h6>`;
+      } //******* "E" row****/
+
+      if (i > 49 && i <= 59) {
+        let n = getLastIDNum;
+        square.innerHTML = `<h6 style="user-select: none">F-${n + 1}</h6>`;
+      } //******* "F" row****/
+
+      if (i > 59 && i <= 69) {
+        let n = getLastIDNum;
+        square.innerHTML = `<h6 style="user-select: none">G-${n + 1}</h6>`;
+      } //******* "G" row****/
+
+      if (i > 69 && i <= 79) {
+        let n = getLastIDNum;
+        square.innerHTML = `<h6 style="user-select: none">H-${n + 1}</h6>`;
+      } //******* "H" row****/
+
+      if (i > 79 && i <= 89) {
+        let n = getLastIDNum;
+        square.innerHTML = `<h6 style="user-select: none">I-${n + 1}</h6>`;
+      } //******* "I" row****/
+
+      if (i > 89 && i <= 99) {
+        let n = getLastIDNum;
+        square.innerHTML = `<h6 style="user-select: none">J-${n + 1}</h6>`;
+      } //******* "J" row****/
+
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  }
+  createBoard(userGrid, userSquares);
+  createBoard(compGrid, compSquares);
+
+  const shipsArray = [
+    {
+      name: "carrier",
+      spaces: [],
+      size: 5,
+    },
+    {
+      name: "battleship",
+      spaces: [],
+      size: 4,
+    },
+    {
+      name: "cruiser",
+      spaces: [],
+      size: 3,
+    },
+    {
+      name: "submarine",
+      spaces: [],
+      size: 3,
+    },
+    {
+      name: "destroyer",
+      spaces: [],
+      size: 2,
+    },
+  ];
+  // let shipLengths = shipsArray.spaces.map((ship) => ship.spaces);
+  // shipLengths;
+  // function taken(nums) {
+  //   squares.splice(nums)
+  // }
+  function randomPosition(squares) {
+    while (shipsArray.length > 0) {
+      let randomNum = Math.floor(Math.random() * squares.length);
+      let axis = Math.random() < 0.5 ? "x" : "y";
+      placeShip(randomNum, axis);
+    }
+  }
+  randomPosition(compSquares);
+  compSquares.length;
+
+  function placeShip(randomNum, axis) {
+    const rN = randomNum;
+
+    let carrierUpNums = "rN, rN - 10, rN - 20, rN - 30, rN - 40";
+    let carrierDownNums = "rN, rN + 10, rN + 20, rN + 30, rN + 40";
+    let carrierLeftNums = "rN, rN - 1, rN - 2, rN - 3, rN - 4";
+    let carrierRightNums = "rN, rN + 1, rN + 2, rN + 3, rN + 4";
+    let battleshipUpNums = "rN, rN - 10, rN - 20, rN - 30";
+    let battleshipDownNums = "rN, rN + 10, rN + 20, rN + 30";
+    let battleshipLeftNums = "rN, rN - 1, rN - 2, rN - 3";
+    let battleshipRightNums = "rN, rN + 1, rN + 2, rN + 3";
+    let cruiserUpNums = "rN, rN - 10, rN - 20";
+    let cruiserDownNums = "rN, rN + 10, rN + 20";
+    let cruiserLeftNums = "rN, rN - 1, rN - 2";
+    let cruiserRightNums = "rN, rN + 1, rN + 2";
+    let submarineUpNums = "rN, rN - 10, rN - 20";
+    let submarineDownNums = "rN, rN + 10, rN + 20";
+    let submarineLeftNums = "rN, rN - 1, rN - 2";
+    let submarineRightNums = "rN, rN + 1, rN + 2";
+    let destroyerUpNums = "rN, rN - 10";
+    let destroyerDownNums = "rN, rN + 10";
+    let destroyerLeftNums = "rN, rN - 1";
+    let destroyerRightNums = "rN, rN + 1";
+
+    if ((axis = "x" && rN % 10 >= 4)) {
+      shipsArray[0].spaces.push(carrierRightNums);
+      shipsArray[1].spaces.push(battleshipRightNums);
+      shipsArray[2].spaces.push(cruiserRightNums);
+      shipsArray[3].spaces.push(submarineRightNums);
+      shipsArray[4].spaces.push(destroyerRightNums);
+    } else if ((axis = "x" && rN % 10 <= 5)) {
+      shipsArray[0].spaces.push(carrierLeftNums);
+      shipsArray[1].spaces.push(battleshipLeftNums);
+      shipsArray[2].spaces.push(cruiserLeftNums);
+      shipsArray[3].spaces.push(submarineLeftNums);
+      shipsArray[4].spaces.push(destroyerLeftNums);
+    } else if ((axis = "y" && rN <= 49)) {
+      shipsArray[0].spaces.push(carrierDownNums);
+      shipsArray[1].spaces.push(battleshipDownNums);
+      shipsArray[2].spaces.push(cruiserDownNums);
+      shipsArray[3].spaces.push(submarineDownNums);
+      shipsArray[4].spaces.push(destroyerDownNums);
+    } else if ((axis = "y" && rN >= 50)) {
+      shipsArray[0].spaces.push(carrierUpNums);
+      shipsArray[1].spaces.push(battleshipUpNums);
+      shipsArray[2].spaces.push(cruiserUpNums);
+      shipsArray[3].spaces.push(submarineUpNums);
+      shipsArray[4].spaces.push(destroyerUpNums);
+    }
+  }
+
+  function shipAlign(randomGenNum) {
+    // if thisNum data.id ends in <4 increment shipsArray
+  }
+}
+startGame();
+// document.addEventListener("load", startGame());
